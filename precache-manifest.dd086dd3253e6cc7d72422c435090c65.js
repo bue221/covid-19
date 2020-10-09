8c5abb54458198f65dac602a036ce8f82aba8624
bue221 <camiloplaza3@gmail.com>
2020-10-08 21:31:29 -0500

    Updates

---
+++ precache-manifest.dd086dd3253e6cc7d72422c435090c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2d6bc44c295055eb28182ffe7946ab",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "0132de07d2d51ff2d7a0",
+    "url": "/covid-19/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "79a9dc8f88d77b6955dd",
+    "url": "/covid-19/static/css/main.1c9210d4.chunk.css"
+  },
+  {
+    "revision": "0132de07d2d51ff2d7a0",
+    "url": "/covid-19/static/js/2.068a2062.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/covid-19/static/js/2.068a2062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a9dc8f88d77b6955dd",
+    "url": "/covid-19/static/js/main.738ca8ab.chunk.js"
+  },
+  {
+    "revision": "44f07a1a748e4a65aa62",
+    "url": "/covid-19/static/js/runtime-main.0df86206.js"
+  }
+]);
